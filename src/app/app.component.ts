fedce1dff024a3a87c1b1ca75db783ede027d778
FelipeACamargo0907 <felipe.camargo0709@gmail.com>
2023-04-28 13:51:57 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Avaliacao_11_04_2023_Angular';
+}
